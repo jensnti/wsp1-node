0b36ae1a06e56ab644594c5f20385a0c89591b17
jensnti <jens.andreasson@ga.ntig.se>
2020-09-29 15:14:41 +0200

    db stuff

--- models/db.js
+++ models/db.js
@@ -1,19 +1,11 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: 'torsdag'
+  database: process.env.DB_DATABASE
 });
 
-connection.connect((error) => {
-  if (error) {
-    throw error;
-  }
-});
-
-module.exports = connection;
-
-// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
-// flush privileges;
+module.exports = pool;
--- routes/test.js
+++ routes/test.js
@@ -1,22 +1,67 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models/db');
+const pool = require('../models/db');
 
-/* GET home page. */
+// const mysql = require('mysql');
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: '127.0.0.1',
+//   user: 'jens',
+//   password: 'Secret123',
+//   database: 'te18'
+// });
+
+// pool.getConnection(function (err, connection) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
+
+//   console.log('connected as id ' + connection.threadId);
+
+//   connection.release();
+// });
+
 router.get('/', function (req, res, next) {
-  const sql = 'SELECT * FROM users';
+  const sql = 'SELECT * FROM meeps';
 
-  db.query(sql, function (err, data, fields) {
+  // pool.getConnection((error, connection) => {
+  //   if (error) {
+  //     throw error;
+  //   }
+  //   console.log('connected as id ' + connection.threadId);
+
+  //   res.send('connected as id ' + connection.threadId);
+
+  //   connection.release();
+  // });
+
+  pool.query(sql, function (err, result, fields) {
     if (err) throw err;
-    // res.json({
-    //   status: 200,
-    //   data,
-    //   message: 'User lists retrieved successfully'
-    // });
-    res.render('test', { data });
+    res.json({
+      status: 200,
+      result
+    });
+    // res.render('test', { result: result });
   });
-
-  // res.render('index', { title: 'Webbserver-programmering med Node.js' });
 });
+// const db = require('../models/db');
+
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   const sql = 'SELECT * FROM meeps';
+
+//   db.query(sql, function (err, rows, fields) {
+//     if (err) throw err;
+//     // res.json({
+//     //   status: 200,
+//     //   data,
+//     //   message: 'User lists retrieved successfully'
+//     // });
+//     res.render('test', { rows: rows });
+//   });
+
+//   // res.render('index', { title: 'Webbserver-programmering med Node.js' });
+// });
 
 module.exports = router;
