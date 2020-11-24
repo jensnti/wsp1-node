ff8f97d1f4fb362f74bb814dd7154daff87f3f8a
jensnti <jens.andreasson@ga.ntig.se>
2020-11-24 15:46:32 +0100

    stylejox

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
-const sassMiddleware = require('node-sass-middleware');
 
 require('dotenv').config();
-
-// const sassMiddleware = require('node-sass-middleware');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -25,13 +22,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true,
-  outputStyle: 'compressed'
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
