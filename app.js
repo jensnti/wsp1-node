b2573db2570b2f51c094305dd6e529a811cef532
jensnti <jens.andreasson@ga.ntig.se>
2020-11-24 20:13:53 +0100

    reworked styles, now readable

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
+const sassMiddleware = require('node-sass-middleware');
 
 require('dotenv').config();
 
@@ -22,6 +23,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true,
+  outputStyle: 'compressed'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,11 +3,38 @@
  * Läs mer, https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event
  */
 window.addEventListener('load', (event) => {
-  const button = document.querySelector('.navbar-toggle');
+  // hämta menyn
   const collapse = document.querySelector('.navbar-collapse');
+  console.log('page is fully loaded');
 
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
-    collapse.classList.toggle('show');
-  }, false);
+  // Hitta knappen i DOM-trädet och spara den i variabeln button.
+  // Ett tips är att kunna och känna till vilka olika selectors vi kan använda.
+  // Vad är ens en selector?
+  // https://developer.mozilla.org/en-US/docs/Glossary/CSS_Selector
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelector
+  const button = document.querySelector('.navbar-toggle');
+
+  // skapa en listener på knappen och bind den till eventet "click"
+  // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+  button.addEventListener(
+    'click',
+    (e) => {
+      e.preventDefault();
+      console.log('Button is pressed');
+      collapse.classList.toggle('collapse');
+
+      // Om du vill byta ikonen för hamburgermeny eller färger så kan du göra det här.
+      // antingen genom att manipulera klasser eller DOM
+      // https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+      // https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+    },
+    false
+  );
+
+  const mql = window.matchMedia('(max-width: 768px)');
+  mql.addEventListener('change', (e) => {
+    if (!e.matches && collapse.classList[1] === 'collapse') {
+      collapse.classList.toggle('collapse');
+    }
+  });
 });
